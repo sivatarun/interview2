b0ffe508394c22b52f74ecda1b6f8c64e15632cd
sivatarun <40629698+sivatarun@users.noreply.github.com>
2018-06-27 17:17:24 +0530

    Update monitor.rb

--- apache2/recipes/monitor.rb
+++ apache2/recipes/monitor.rb
@@ -12,14 +12,18 @@ end
 
 execute 'install scripts' do
   command 'curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -o'
+  notifies :run, 'execute[unzip scripts]', :immediately
 end
 
 execute 'unzip scripts' do
   command 'unzip CloudWatchMonitoringScripts-1.2.2.zip'
+  action :nothing
+  notifies :run, 'execute[change directory]', :immediately
 end
 
 execute 'change directory' do
   command 'cd aws-scripts-mon'
+  action: nothing
 end
 
 
